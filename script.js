b498bb0b75ff5ddbf54c0235750ee899172f3b9f
Mate Tanczos <mtanczos79@gmail.com>
2020-07-29 08:16:13 -0400

    added checkbox and textbox

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Write password to the #password input
 function writePassword() {
-  let keyListLower = 'abcdefghijklmnopqrstuvwxyz';
-  let keyListUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let keyListNum = '0123456789';
-  let keyListSpecial = "@%+/'!#$^?:.(){}[]~";
+  let keyListLower = ['abcdefghijklmnopqrstuvwxyz'];
+  let keyListUpper = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  let keyListNum = ['0123456789'];
+  let keyListSpecial = ['!@#$%^&*'];
   let genPassword = '';
   let useLower = false;
   let useUpper = false;
@@ -11,7 +11,10 @@ function writePassword() {
   let useSpecial = false;
   let charsToUse = '';
 
-  var perPassLength = prompt("Please enter your password length - has to be > 8 but < 128 chars", 8);
+
+  perPassLength = document.getElementById("passWordLength").value
+
+  //var perPassLength = prompt("Please enter your password length - has to be > 8 but < 128 chars", 8);
   //validate length input
   if (isNaN(perPassLength)) { //check if user entered a number
     alert('Characters used must be a number!')
@@ -23,23 +26,22 @@ function writePassword() {
   }
 
   //ask user if they want to use lower
-  var addLowerCaseChars = confirm('Do you want to use lowercase characters?  OK= Yes, Cancel=No');
-  if (addLowerCaseChars) {
+  if (document.getElementById("lowerCase").checked) {
     useLower = true;
   }
+
   //ask user if they want to use Upper
-  var addUpperCaseChars = confirm('Do you want to use uppercase characters?  OK= Yes, Cancel=No');
-  if (addUpperCaseChars) {
+  if (document.getElementById("upperCase").checked) {
     useUpper = true;
   }
+
   //ask user if they want to use Number
-  var addNumbers = confirm('Do you want to include numbers?  OK= Yes, Cancel=No');
-  if (addNumbers) {
+  if (document.getElementById("numbers").checked) {
     useNum = true;
   }
+
   //ask user if they want to use special chars
-  var addSpecialChars = confirm('Do you want to include special characters?  OK= Yes, Cancel=No');
-  if (addSpecialChars) {
+  if (document.getElementById("specialChars").checked) {
     useSpecial = true;
   }
 
@@ -65,18 +67,18 @@ function writePassword() {
     genPassword += charsToUse.charAt(Math.floor(Math.random() * charsToUse.length));
   }
 
-  document.getElementById("password").placeholder = genPassword;
+  document.getElementById("password").value = genPassword;
 }
 
-function copyText(){
-  //document.getElementById("password").select();
-  //document.execCommand('copy');
- 
-    document.getElementById("password").select();
-
+function copyToClipboard() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert(`Copied the text:  ${copyText.value} to the clipboard`);
 }
+
 //***********EVENT LISTENERS ****************************************************
 // Add event listener to generate button
 document.getElementById("generate").addEventListener("click", writePassword);
-
-document.getElementById("copyTxt").addEventListener("click", copyText);
+document.getElementById("copyTxt").addEventListener("click", copyToClipboard);
