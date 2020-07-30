6591e5992d08d3ff4eecf4dc40c9c3624c68471b
Mate Tanczos <mtanczos79@gmail.com>
2020-07-30 18:00:27 -0400

    add comments, added screenshots, and updated Read me

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Password_Generator
+
+> This project is to showcase how javascript can interact with the web browser using DOM.
+The project uses a validated textbox so users can enter in the length of the password they want to generate.
+Users can also select if they want to use lower, upper, numbers, and/or special characters.
+The code will then generate a password if the length enter in between 8 and 128 chars, and one checkbox must checked, other wise the code will stop
+
+## Table of contents
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Features](#features)
+* [Features](#Acknowledgments)
+
+## Screenshots
+![Example screenshot](Screenshots/screenshot1.jpg)
+
+## Technologies
+* Bootstrap 4.5
+* HTML5
+* CSS3
+* Javascript
+
+## Features
+List of features I incorparated
+* interactive password form
+* usable textbox and checkboxes to interact with browser and javascript
+
+
+## Acknowledgments:
+* https://www.w3schools.com/js/default.asp
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Write password to the #password input
+//Created by: Mate' Tanczos
+//Homework #3
+//Create Date: Tuesday, ‎July ‎28, ‎2020, ‏‎12:14:05 PM
+// set key vars, and initialize vars
 function writePassword() {
   let keyListLower = ['abcdefghijklmnopqrstuvwxyz'];
   let keyListUpper = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
@@ -11,65 +14,62 @@ function writePassword() {
   let useSpecial = false;
   let charsToUse = '';
 
-
+//look at passWordLength ID to get what user entered in for the length of the password
   perPassLength = document.getElementById("passWordLength").value
 
-  //var perPassLength = prompt("Please enter your password length - has to be > 8 but < 128 chars", 8);
   //validate length input
-  if (isNaN(perPassLength)) { //check if user entered a number
+  if (isNaN(perPassLength)) { //check if user entered a number, if not alert msg and stop the code from running
     alert('Characters used must be a number!')
     return; //stops code
   }
-  if (perPassLength < 8 || perPassLength > 128) { //checks if user used correct lenght
+  if (perPassLength < 8 || perPassLength > 128) { //checks if user used correct length, if its outside the range then show alert and stop the code
     alert('Characters used is not within the specified limits!')
     return; //stops code
   }
 
-  //ask user if they want to use lower
+  //see if checkbox is checked for #lowerCase
   if (document.getElementById("lowerCase").checked) {
     useLower = true;
+    charsToUse += keyListLower; //add all lower case letters to charsToUse var
   }
 
-  //ask user if they want to use Upper
+  //see if checkbox is checked for #upperCase
   if (document.getElementById("upperCase").checked) {
     useUpper = true;
+    charsToUse += keyListUpper; //add all upper case letters to charsToUse var
   }
 
-  //ask user if they want to use Number
+   //see if checkbox is checked for #numbers
   if (document.getElementById("numbers").checked) {
     useNum = true;
+    charsToUse += keyListNum; //add all numbers to charsToUse var
   }
 
-  //ask user if they want to use special chars
+   //see if checkbox is checked for #specialChars
   if (document.getElementById("specialChars").checked) {
     useSpecial = true;
+    charsToUse += keyListSpecial; //add all special chars available to charsToUse var
   }
 
+  //check if user has checked any of the checkboxes, if not alert user and stop the code
   if (useLower == false && useUpper == false && useNum == false && useSpecial == false) {
     alert("Must select a parameter to generate a password");
     return;
   }
 
-  if (useLower) {
-    charsToUse += keyListLower;
-  }
-  if (useUpper) {
-    charsToUse += keyListUpper;
-  }
-  if (useNum) {
-    charsToUse += keyListNum;
-  }
-  if (useSpecial) {
-    charsToUse += keyListSpecial;
-  }
-
+  //using the length of the password lenght set loop count
   for (i = 0; i < perPassLength; i++) {
+    //The charAt() method returns the character at the specified index in a string.
+    //using the length of the combantions of lower, upper, number or special chars to get the random pick for ech time to for loop is excuted
+    //using the var genPassword to hold the random pick
     genPassword += charsToUse.charAt(Math.floor(Math.random() * charsToUse.length));
   }
 
+  //once the loop is done display the password in the #password ID field
   document.getElementById("password").value = genPassword;
 }
 
+//this function is use to copy the the #password field and copy it to the clipboard
 function copyToClipboard() {
   var copyText = document.getElementById("password");
   copyText.select();
@@ -79,6 +79,6 @@ function copyToClipboard() {
 }
 
 //***********EVENT LISTENERS ****************************************************
-// Add event listener to generate button
+// Event listener for gen password , and copy to clipboard button
 document.getElementById("generate").addEventListener("click", writePassword);
 document.getElementById("copyTxt").addEventListener("click", copyToClipboard);
